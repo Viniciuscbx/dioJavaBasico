92bca39b678d74a51233662776b9d2eaea11416b
Viniciuscbx <99989065+Viniciuscbx@users.noreply.github.com>
2024-06-12 12:24:43 -0300

    app de gerencia bancaria

---
+++ ContaBanco/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public ContaTerminal {
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta:");
+        int numero = entrada.nextInt;
+
+        System.out.println("Digite o número da Agência e dígito:");
+        String agencia = entrada.nextLine;
+
+        System.out.println("Digite o nome do titular da conta:");
+        String nomeCliente = entrada.nextLine;
+
+        double saldo = 237.48;
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta 
+        em nosso banco, sua agência é " + agencia +", conta " + numero + " e seu saldo de " +
+        saldo + " já está disponível para saque.");
+
+        entrada.close();
+    }
+}
