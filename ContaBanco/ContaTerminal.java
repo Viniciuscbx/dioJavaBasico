d8b676f1362871236e6d061a0801946182d2c0d3
Viniciuscbx <99989065+Viniciuscbx@users.noreply.github.com>
2024-07-05 21:28:59 +0000

    atualização

--- ContaBanco/ContaTerminal.java
+++ ContaBanco/ContaTerminal.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-public ContaTerminal {
+public class ContaTerminal {
     
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Digite o número da conta:");
-        int numero = entrada.nextInt;
+        int numero = entrada.nextInt();
 
         System.out.println("Digite o número da Agência e dígito:");
-        String agencia = entrada.nextLine;
+        String agencia = entrada.nextLine();
 
         System.out.println("Digite o nome do titular da conta:");
-        String nomeCliente = entrada.nextLine;
+        String nomeCliente = entrada.nextLine();
 
         double saldo = 237.48;
 
